a27096c5d713730b13d1f639aa1fba26853698e1
hello1226-bit <nd30215@gmail.com>
2019-10-21 02:27:25 +0900

    Scene 작업 추가
    Scene 기본 틀 작업 완료했습니다. 확인부탁드립니다.

---
+++ Assets/TitleScrpit/TitleButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("01_Stage");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

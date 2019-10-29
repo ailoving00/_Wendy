c10d6e7633f546671c7bbdcffd2b74a9845d93e3
baehuiji <56417504+baehuiji@users.noreply.github.com>
2019-10-30 00:43:45 +0900

    퍼즐 조작 관련

--- Assets/Scrpits/Stage1/Controller/mouse_test.cs
+++ Assets/Scrpits/Stage1/Controller/mouse_test.cs
@@ -1,6 +1,8 @@
 ﻿//
-//2019-10-22
+//2019-10-29
 //자동차 퍼즐 조작
+//바뀐 블럭 움직임 : 블럭을 클릭, 드래그한 방향으로 130. 
+//                -> (기존의 블럭 움직임 : 블럭을 클릭하고 끌어다 움직이는 대로)
 //
 using System.Collections;
 using System.Collections.Generic;
@@ -12,28 +14,49 @@ public class mouse_test : MonoBehaviour
 
     private GameObject target = null; //mouse target
 
-    private GameObject _blockPrefab;
+    //private GameObject _blockPrefab;
     private GameObject[] _blocks;
-    public GameObject _exit; //탈출구
-    public GameObject _me; //옮겨야할 블럭
+    public GameObject _exit; //없어도 됨.
+    public GameObject _me;
 
     private Vector3 MousePos;
-    private bool _targetState = false;
+    private bool _targetState;
+
+    private Vector3 startPos;
+    private Vector3 lastPos;
+    private bool mouseUp;
+    private bool mouseDrag;
+    private Vector3 direction;
+
+    private Block blockScript;
+
+    private Vector3 subVecs;
+    private Vector3 absVec;
+    private float subXY;
 
     void Start()
     {
         if (_blocks == null)
             _blocks = GameObject.FindGameObjectsWithTag("Block");
 
-        //foreach (GameObject block in _blocks)
-        //{
-        //    Instantiate(_blockPrefab, block.transform.position, block.transform.rotation);
-        //}
+        _targetState = false;
+
+        startPos = Vector3.zero;
+        lastPos = Vector3.zero;
+        direction = Vector3.zero;
+        mouseUp = false;
+        mouseDrag = false;
+
+        blockScript = null;
+
+        subVecs = Vector3.zero;
+        absVec = Vector3.zero;
+        subXY = 0f;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //OnMouseDown()
         {
             target = GetClickedObject();
 
@@ -41,24 +64,75 @@ public class mouse_test : MonoBehaviour
             {
                 if (target.tag == "Block")
                 {
-                    _targetState = true;
-
                     Debug.Log(target.GetComponent<Collider>().name);
+
+                    _targetState = true; //타겟 선택
+                    mouseDrag = true;
+
+                    blockScript = target.GetComponent<Block>();
+
+                    startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //혹은 target 의 위치
                 }
             }
         }
-        else if (true == Input.GetMouseButtonUp(0))
+        else if (true == Input.GetMouseButtonUp(0)) //OnMouseUp(), 단한
         {
-            _targetState = false;
+            if (_targetState)
+            {
+                mouseUp = true;
+
+                mouseDrag = false;
+                lastPos = MousePos;
+
+                _targetState = false;
+            }
         }
 
-        if (true == _targetState)
+        if (true == mouseDrag) //드래그를 한 상태
         {
             OnMouseDrag();
         }
-        else
-        {
 
+        if (true == mouseUp)
+        {
+            subVecs = lastPos - startPos;
+
+            absVec = subVecs;
+            ChangeAbsolutevalue(ref absVec);
+
+            subXY = absVec.x - absVec.y;
+
+            if (subXY >= 0) //x축 이동
+            {
+                if (subVecs.x >= 0)
+                {
+                    direction = new Vector3(1f, 0f, 0f);
+                    blockScript.Move(direction, 0);
+                }
+                else
+                {
+                    direction = new Vector3(-1f, 0f, 0f);
+                    blockScript.Move(direction, 0);
+                }
+            }
+            else //y축 이동
+            {
+                if (subVecs.y > 0)
+                {
+                    direction = new Vector3(0f, 1f, 0f);
+                    blockScript.Move(direction, 1);
+                }
+                else
+                {
+                    direction = new Vector3(0f, -1f, 0f);
+                    blockScript.Move(direction, 1);
+                }
+            }
+
+            //각도 구하기
+            //AngleInDeg(startPos, myPos);
+
+            mouseUp = false;
         }
     }
     //void OnMouseDown() {}
@@ -79,15 +153,39 @@ public class mouse_test : MonoBehaviour
         return target;
     }
 
-    private Vector3 OnMouseDrag()
+    void OnMouseDrag()
     {
+        //Debug.Log("Drag!!"); //print("Drag!!");
+        //print(MousePos);
+
+        //마우스 포인터 위치
         MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
                                                               Input.mousePosition.y,
                                                               -Camera.main.transform.position.z));
-        target.transform.position = new Vector3(MousePos.x, MousePos.y, target.transform.position.z);
+        //Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
 
-        //print(MousePos);
+        //이동
+        //target.transform.position = new Vector3(MousePos.x, MousePos.y, target.transform.position.z);
+    }
 
-        return MousePos;
+    public void ChangeAbsolutevalue(ref Vector3 vec) //Mathf.Abs(n)
+    {
+        if (vec.x < 0)
+            vec.x = vec.x * -1;
+
+        if (vec.y < 0)
+            vec.y = vec.y * -1;
+
+        //z는 안 해도됨.
+    }
+
+    public static float AngleInRad(Vector3 vec1, Vector3 vec2)
+    {
+        return Mathf.Atan2(vec2.y - vec1.y, vec2.x - vec1.x);
+    }
+
+    public static float AngleInDeg(Vector3 vec1, Vector3 vec2)
+    {
+        return AngleInRad(vec1, vec2) * 180 / Mathf.PI;
     }
 }

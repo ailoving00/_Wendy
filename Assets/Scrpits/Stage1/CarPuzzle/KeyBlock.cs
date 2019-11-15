1d6651a5003fefd2b34d43a0a724d2ea5d177dee
hello1226-bit <nd30215@gmail.com>
2019-11-15 17:40:43 +0900

    Revert "Lightpuzzle_add"
    This reverts commit f8e52a8c061f55a0311a108af97aee6b613423ad.

---
+++ Assets/Scrpits/Stage1/CarPuzzle/KeyBlock.cs
@@ -0,0 +1,27 @@
+﻿//
+//2019-11-14
+//도착지에 들어가야하는 블럭
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBlock : MonoBehaviour
+{
+    public GameObject winText;
+
+    void start()
+    {
+        //GetComponent<Block>().bType = 0;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Exit")
+        //if (coll.gameObject.CompareTag("Exit"))
+        {
+            winText.SetActive(true);
+            Debug.Log("__Exit__");
+        }
+    }
+}

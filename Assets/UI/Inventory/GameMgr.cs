a08eb3ed465391ac8d95208da10b0e7d1335b877
hello1226-bit <nd30215@gmail.com>
2019-12-13 16:46:51 +0900

    eyeside 씬 제거 완료
    업로드합니다. eyeside 씬은 따로 빼서 작업예정입니다.
    2개의 상자 에셋이 들어있습니다!

--- Assets/UI/Inventory/GameMgr.cs
+++ Assets/UI/Inventory/GameMgr.cs
@@ -13,6 +13,15 @@ public class GameMgr : MonoBehaviour
     //일시정지 여부를 판단하는 변수
     private bool isPaused;
 
+    //인벤토리 패널을 가져와 부드러운 이동 시전; 
+    public GameObject Inventory_Panel;
+
+    // 스크롤 트랜스 폼을 받아오는 함수
+    public RectTransform scrollRectView;
+
+
+
+
 
     //Inventory 의 CanvasGroup 컴포넌트를 저장할 변수 
     public CanvasGroup inventoryCG;
@@ -25,6 +34,14 @@ public class GameMgr : MonoBehaviour
     {
         OnInventoryOpen(false);
         check = false;
+
+        RectTransform scrollRectView = GetComponent<RectTransform>();
+        RectTransform Inventory_Panel = GetComponent<RectTransform>();
+
+        //Vector2 Inventory_false = Inventory_Panel.anchoredPosition - new Vector2(46, 0);
+       // Vector2 Inventory_true = Inventory_Panel.anchoredPosition + new Vector2(46, 0);
+       // 부드러운 이동은 ~ 언제나 애니메이션이 답이야! 
+
     }
 
     // Update is called once per frame
@@ -34,12 +51,42 @@ public class GameMgr : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (check)
+            {
                 check = false;
-            else
-                check = true;
+                SlideInventory(false);
 
-            OnInventoryOpen(check);
+                //Inventory_Panel.anchoredPosition = Vector2.Lerp(Inventory_Panel.anchoredPosition, Inventory_false, Time.deltaTime); 
+            }
+            else
+            {
+                check = true;
+                SlideInventory(true);
+                //Inventory_Panel.anchoredPosition += new Vector2(46, 0);
+                // Inventory_Panel.anchoredPosition = Vector2.Lerp(Inventory_Panel.anchoredPosition, Inventory_true, Time.deltaTime);
+            }
+
+                OnInventoryOpen(check);
         }
+
+        if (check == true)
+        {
+
+            
+            //panel 위치가 움직이여함
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                Debug.Log("위로 스크롤 확인 ");
+                scrollRectView.anchoredPosition += new Vector2(0, -10);
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                Debug.Log("아래로 스크롤 확인 ");
+                scrollRectView.anchoredPosition += new Vector2(0, 10);
+            }
+        }
+
     }
 
     public void OnPauseClick()
@@ -60,22 +107,28 @@ public class GameMgr : MonoBehaviour
         {
              scripts.enabled = !isPaused;
          }
-               
-    */
-
-        //똑같이 했는데 안돼
-        //확장메서드있는데 없다고 떠
-        //1시간 삽질 실화냐ㅐ 
-        //일부 UI도 정지되게 구현 가능 - 추후 구현-
+               */
     }
 
     public void OnInventoryOpen(bool isOpened)
     {
+        /*
         inventoryCG.alpha = (isOpened) ? 1.0f : 0.0f;
         inventoryCG.interactable = isOpened;
         inventoryCG.blocksRaycasts = isOpened;
+        */
 
+    }
 
+    public void SlideInventory(bool is2Opened)
+    {
+        Animator animator = Inventory_Panel.GetComponent<Animator>();
+        if (Inventory_Panel != null)
+        {
+            bool isOpen = animator.GetBool("open");
+
+            animator.SetBool("open", !isOpen);
+        }
     }
 
     

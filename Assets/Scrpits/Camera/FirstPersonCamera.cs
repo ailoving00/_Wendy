d2dbaff0063d6052e6f5d17d82be002528e0f43b
baehuiji <56417504+baehuiji@users.noreply.github.com>
2020-01-18 20:00:14 +0900

    외곽선/2스테이지 카메라,캐릭터이동

---
+++ Assets/Scrpits/Camera/FirstPersonCamera.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public Transform target;
+    public float dist = 0.9f;
+
+    public float xSpeed = 100.0f; //220.0f;
+    public float ySpeed = 100.0f;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 60f;
+
+    public bool jumpScare_state = false; //화면 고정, translateTest에서 값 바뀜
+
+    float m_FieldOfView = 50f; //50f, 30f
+    public Camera cameraOption;
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //커서 고정
+        Vector3 angles = transform.eulerAngles;
+
+        x = angles.y;
+        y = angles.x;
+    }
+
+    void Update()
+    {
+        if (jumpScare_state)
+        {
+            m_FieldOfView = 30f;
+            cameraOption.fieldOfView = m_FieldOfView;
+
+            return;
+        }
+
+        m_FieldOfView = 50f;
+        cameraOption.fieldOfView = m_FieldOfView;
+
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.015f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.015f;
+
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 position = rotation * new Vector3(dist, dist, dist) + target.position;
+
+        transform.rotation = rotation;
+        target.rotation = Quaternion.Euler(0, x, 0);
+        transform.position = position;
+    }
+
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    if(col.gameObject.tag == "JumpScare") //뻐꾸기애니메이션이 끝났을때
+    //    {
+    //        Debug.Log("col");
+    //    }
+    //}
+}
---
+++ Assets/Scrpits/Character/Player_HJ.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_HJ : MonoBehaviour
+{
+    private Vector3 min, max;
+    private float h, v;
+
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+
+    Animator _animator = null;
+
+    public bool gameEnd;
+
+    public GameObject playerObj; //벡터
+    public GameObject playerObj_normal; //법선벡터
+
+    private Vector3 targetForward;
+    private Vector3 targetNormal_Forward;
+
+    void Start()
+    {
+        _animator = GameObject.Find("wendy_umuni_rigging").GetComponent<Animator>();
+
+        gameEnd = false;
+    }
+
+    //protected override void Update() //활동제한
+    //{
+    //    //Executes the GetInput function
+    //    GetInput();
+
+    //    float xMinClamp = Mathf.Clamp(transform.position.x, min.x, max.x);
+    //    float yMinClamp = Mathf.Clamp(transform.position.y, min.y, max.y);
+
+    //    transform.position = new Vector3(xMinClamp, yMinClamp, transform.position.z);
+    //}
+    void Update()
+    {
+        if (gameEnd)
+            return;
+
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        targetForward = playerObj.transform.rotation * Vector3.forward; //localRotation -> rotation
+        targetNormal_Forward = playerObj_normal.transform.rotation * Vector3.forward;
+
+
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        // - Animation transition
+        if (h == 0 && v == 0)
+        {
+            _animator.SetBool("IsWalking", false);
+            //Debug.Log("Arrow Button Released");
+        }
+        else
+        {
+            _animator.SetBool("IsWalking", true);
+
+            targetForward = playerObj.transform.rotation * Vector3.forward; //localRotation -> rotation
+        }
+
+        // - 디버그
+        Debug.DrawRay(playerObj.transform.position, targetForward, Color.blue);
+
+        // - 움직임2
+        //앞뒤
+        if (v == 1)
+        {
+            transform.Translate(-targetForward.x * movementSpeed * Time.deltaTime, 0, -targetForward.z * movementSpeed * Time.deltaTime);
+        }
+        else if (v == -1)
+        {
+            transform.Translate(targetForward.x * movementSpeed * Time.deltaTime, 0, targetForward.z * movementSpeed * Time.deltaTime);
+        }
+        //좌우
+        if (h == 1)
+        {
+            transform.Translate(-targetNormal_Forward.x * movementSpeed * Time.deltaTime, 0, -targetNormal_Forward.z * movementSpeed * Time.deltaTime);
+        }
+        else if (h == -1)
+        {
+            transform.Translate(targetNormal_Forward.x * movementSpeed * Time.deltaTime, 0, targetNormal_Forward.z * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    public void SetLimits(Vector3 min, Vector3 max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}

9e4c72cdfa7bf872c2043fb43c83aedda0f2b564
hello1226-bit <56398760+hello1226-bit@users.noreply.github.com>
2020-03-22 10:28:34 +0900

    1Stage 충돌영역 수정
    1스테이지 충돌영역 검사를 수정했습니다!

--- Assets/Scrpits/Inventory/ActionController_01.cs
+++ Assets/Scrpits/Inventory/ActionController_01.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 
 
@@ -12,16 +13,33 @@ public class ActionController_01 : MonoBehaviour
     public Inventory theInventory;
 
     [SerializeField]
-    private float range;
+    private float range; // 충돌 체크 구의 반경
+    [SerializeField]
+    private float length = 3f ; // 충돌 체크의 최대 거리 
 
     [SerializeField]
     private LayerMask layerMask;
 
     [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private GameObject P_target;//타겟 발 오브젝트와 연결된 pivot
+
+    public float angleRange = 180f;
+    public float distance = 5f;
+    public bool isCollision = false;
+    Color _blue = new Color(0f, 0f, 1f, 0.2f);
+    Color _red = new Color(1f, 0f, 0f, 0.2f);
+    Vector3 direction;
+    float dotValue = 0f;
+
+
+    [SerializeField]
     private Text actionText;
 
     /// acquire true - false 
-    private bool pickupActivated = false;
+    public bool pickupActivated = false;
     private RaycastHit hitInfo;
 
     // item event
@@ -39,7 +57,18 @@ public class ActionController_01 : MonoBehaviour
     {
         CheckItem();
         TryAction();
+
     }
+    private void OnDrawGizmos()
+    {
+        Handles.color = isCollision ? _red : _blue;
+        Handles.DrawSolidArc(target.transform.position, Vector3.up, target.transform.forward, angleRange / 2, distance); 
+        //                     타겟에의 위치에서, 타겟의 위치 앞전방으로,위아래 판별, 각도는 몇만큼, 방향은 몇만큼
+        Handles.DrawSolidArc(target.transform.position, Vector3.up, target.transform.forward, -angleRange / 2, distance);
+        //                     타겟에의 위치에서, 타겟의 위치 앞전방으로 ,위아래 판별, 각도는 몇만큼의 -, 방향은 몇만큼. 
+
+    }
+
 
 
     private void TryAction()
@@ -82,25 +111,65 @@ public class ActionController_01 : MonoBehaviour
         }
     }
 
+
     private void CheckItem()
     {
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfo, range, layerMask))
+        if (Physics.SphereCast(P_target.transform.position, range, Vector3.up , out hitInfo, length, layerMask))
         {
-            if (hitInfo.transform.tag == "Item")
+            //                 레이저 발사 위치            , 구의 반경, 발사 방향,      충돌 결과,     최대거리, 레이어마스크
+            ItemInfoAppear();
+            CheckSectorform();
+
+            Debug.Log("검토작업시작");
+            if (isCollision)
             {
-                ItemInfoAppear();
+                if (hitInfo.transform.tag == "Item")
+                {
+                    ItemInfoAppear();
+                    Debug.Log("이건 되고있나?");
+                }
+
+                if (hitInfo.transform.tag == "ItemSet")
+                {
+                    DoorBlockSet();
+                }
+
             }
 
-            if (hitInfo.transform.tag == "ItemSet")
-            {
-                DoorBlockSet();
-            }
+
         }
+
         else
+        {
             InfoDisappear();
+            isCollision = false;
+            
+        }
+
+
     }
 
 
+    private void CheckSectorform()
+    {
+        dotValue = Mathf.Cos(Mathf.Deg2Rad * (angleRange / 2)); //코사인을 구하는것....안의 각도를 구하는것
+        direction = hitInfo.transform.position - target.transform.position; 
+        if (direction.magnitude < distance) 
+        {
+            if (Vector3.Dot(direction.normalized, target.transform.forward) > dotValue) 
+            {
+                isCollision = true;
+                Debug.Log("충돌중");
+            }
+            else
+            {
+                isCollision = false;
+                Debug.Log("충돌안함");
+            }
+        }
+        else
+            isCollision = false;
+    }
 
 
     // Need to modify
